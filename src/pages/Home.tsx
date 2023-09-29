a54dde110bf477e7074df74d67c37d334aae56dd
Shivam Kumar <shivam-kumar-shah@outlook.com>
2023-09-29 13:32:10 +0530

    build: :sparkles: add dev script

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,9 @@ const Home = (props: Props) => {
   return (
     <div className="w-screen h-screen overflow-y-hidden">
       <Header />
-      <main className="flex flex-row h-full relative pt-20">
+      <main className="flex flex-row h-full relative pt-20 w-full">
         <Nav />
-        <div className="pl-64 h-full bg-surface w-fit min-h-full">
+        <div className="pl-64 h-full bg-surface w-fit min-h-full min-w-full">
           <section className="p-6 flex flex-row gap-x-4 min-w-fit">
             <BoardSection />
             <BoardSection />
